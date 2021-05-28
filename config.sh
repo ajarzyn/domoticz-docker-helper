deb4db7e7e3755713485c04fa7360176e558bfc8
Andrzej Jarzynka <jarzyn15@gmail.com>
2021-05-28 23:42:48 +0200

    config.sh file created. Added tag option. Added create container test on target. Fix for container ports. No longer password needed for image scp. Fix loading image. Fix for image names.

---
+++ config.sh
@@ -0,0 +1,6 @@
+# Server paramteres
+IP="" # IP of the server
+SSH_PORT="" # Port for ssh 
+USER="" # NAS/Server user
+SERVER_PATH="" # Path for domoticz where is config file (in which there is db/plugins)
+SERVER_SCRIPT_PATH=""
